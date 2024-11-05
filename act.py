623e05d94ed445fcffb0d39ac4e3869cfea5d9b6
Joseph Garcia <32553175+josephgarcia01@users.noreply.github.com>
2024-11-05 14:25:54 -0600

    Changed old script to new script
    VERY VERY rough draft updated to the most recent version.

--- act.py
+++ act.py
@@ -1,86 +1,67 @@
-from pynput.keyboard import Key, Controller
-from time import sleep
-import webbrowser
-import pyautogui
+##Opens an RDP session and checks if the window is still active.
+##Author: jg
+##Date: 2/2023
+
 import time
 import os
-##any instance of time.sleep(x) is for 
-##pausing the Pi before doing the next action
-keyboard = Controller()
+import pyautogui
+import subprocess
+
 pyautogui.FAILSAFE = False
 wh = pyautogui.size()
 wide = wh.width
 high = wh.height
 wide1 = wide % 16
 
-def search():
-##opens browser/fullscreen
-	time.sleep(2)
-	webbrowser.open_new("chrome://newtab") #opens buffer window
-	time.sleep(5)
-	pyautogui.hotkey('ctrl','shift','n') #opens main window 
-	time.sleep(2)
-	pyautogui.hotkey('alt','\t')
-	time.sleep(1)
-	pyautogui.hotkey('alt','f4')
-	time.sleep(2)
-	pyautogui.write("https://ccx1.dhrhs.com:8444/cuicui/permalink/?viewId=D89EC20E10000175000274F00A7F0838&linkType=dashboard") #edit link to specific website
-	time.sleep(4)
-	pyautogui.press('enter')
-	time.sleep(3)
-	pyautogui.press('f11')
+#tells user what pi is opening
+#runs rdesktop
+def open():
+    time.sleep(10)
+    pyautogui.hotkey('ctrl','alt','t')
+    time.sleep(5)
+    pyautogui.write("stty -echo ##Starting Bedtracker")
+    pyautogui.press('enter')
+    time.sleep(5)
+    pyautogui.write("rdesktop ACCOUNT_NAME -d DOMAIN -u USER -p 'PASS'")
+    time.sleep(4)
+    pyautogui.press('enter')
+    time.sleep(7)
 
-def keyb():
-##moves using keyboard strokes
-##initial bootup navigation
-	pyautogui.write('21943') #inputs username
-	for i in range(1):
-		pyautogui.press('\t')
-		time.sleep(3)
-	pyautogui.press('enter')
-	time.sleep(8)
-	pyautogui.write('21943') #inputs password
-	for i in range(1):
-		time.sleep(3)
-		pyautogui.press('\t')
-		time.sleep(2)
-	pyautogui.press('enter')
-	time.sleep(30)
-	for i in range(2):
-		pyautogui.press('\t')
-		time.sleep(2)	
-	pyautogui.press('space')
+#runs shell command to return true/false if window is active
+def check_window_open(window_title):
+    cmd = ['xdotool', 'search', '--onlyvisible', '--name', window_title]
+    try:
+        subprocess.check_output(cmd)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
-def zoom():
-#for specific displays, it needs to zoom out
-	time.sleep(3)
-	if wide == 1366:
-		time.sleep(5)
-		for i in range(4):
-			pyautogui.hotkey('ctrl','-') #zooms the screen out for weird resolution screen
-			time.sleep(2)
+def restart_linux():
+    subprocess.call(['sudo', 'reboot'])
 
-time.sleep(5)
-search()
-time.sleep(30)
-keyb()
-time.sleep(6)
-zoom()
+#assigns window that needs to be open to variable
+window_title = "nameofWindow"
 
-##moves mouse to edge of screen
+open()
+time.sleep(3)
+for x in range(2):
+    pyautogui.press('tab')
+pyautogui.press('enter')
+
 if wide1 == 0:
-	hl = high / 2
-	pyautogui.moveTo(wide, hl)
+    h1 = high / 2
+    pyautogui.moveTo(wide, h1)
 else:
-	hl = high / 2
-	pyautogui.moveTo(wide, hl)
+    h1 = high / 2
+    pyautogui.moveTo(wide, h1)
 
-##keeps mouse moving up and down
+#loops continuously; if window expectedly shuts down
+#due to disconnect, restart.
 while True:
-	now = time.strftime("%H:%M:%S", time.localtime())
-	time.sleep(4)
-	pyautogui.moveTo(wide, hl-10, duration=1)
-	time.sleep(4)
-	pyautogui.moveTo(wide, hl+10, duration=1)
-	if (now == "04:00:00"):			#at 4am, hits the refresh button
-		pyautogui.press('f5')
+    time.sleep(3)
+    pyautogui.moveTo(wide, h1-10, duration = 1)
+    time.sleep(5)
+    pyautogui.moveTo(wide, h1+10, duration = 1)
+    if not check_window_open(window_title):
+        print("The window is not open. Restarting Linux...")
+        restart_linux()
